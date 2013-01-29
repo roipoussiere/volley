0e058624f43561f11e7b164a3f9238b734107aa5
julienA974 <maniak.974@hotmail.fr>
2013-01-29 23:57:56 +0100

    Update src/v_vues/Vue_Lecture.java

--- src/v_vues/Vue_Lecture.java
+++ src/v_vues/Vue_Lecture.java
@@ -1,19 +1,29 @@
 package v_vues;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.*;
+
+import v_ecouteurs.AL_Fenetre;
 
 import c.Controleur;
 
 /**
  * Vue contenant les élements de l'onglet Lecture.
+ * @author Julien Ampouta
  */
 @SuppressWarnings("serial")
 public class Vue_Lecture extends JPanel
 {
 	private Controleur c;
-	
+
 	// Composants de la fenêtre
-	
+	private SelectionTemps selecTps ;
+	private JComboBox typeStrategie, choixEquipe ;
+	private JLabel libelleFichier ;
+	private JButton  lecture, stop, pleinEcran;
 
 	/**
 	 * Création du JFrame et de tous ses composants.
@@ -23,8 +33,59 @@ public class Vue_Lecture extends JPanel
 	{
 		super ();
 		this.c = _c;
+		
+
+		// Mise en place du GridBagLayout
+		this.setLayout (new GridBagLayout()) ;
+		GridBagConstraints gbc = new GridBagConstraints () ;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+
+		// Sélection du temps
+		this.selecTps = new SelectionTemps () ;
+		gbc.gridx = 0 ; gbc.gridy = 1 ;
+		gbc.gridwidth = 3 ; gbc.gridheight = 1 ;
+		gbc.anchor = GridBagConstraints.PAGE_START ;
+		gbc.insets = new Insets (0, 0, 0, 0) ;
+		this.add (this.selecTps, gbc) ;
+
+		// Choix équipe
+		this.choixEquipe = new JComboBox () ;
+		this.choixEquipe.setEditable (false) ;
+		this.choixEquipe.addItem ("Equipe 1") ;
+		this.choixEquipe.addItem ("Equipe 2") ;
+		gbc.anchor = GridBagConstraints.FIRST_LINE_END ;
+		gbc.insets = new Insets (0, 135, 10, 10) ;
+		this.add (this.choixEquipe, gbc) ;
+		
+		// Nom fichier
+		this.libelleFichier = new JLabel ("Nom du fichier... ") ;
+		gbc.anchor = GridBagConstraints.CENTER ;
+		gbc.insets = new Insets (0, 0, 0, 0) ;
+		this.add (this.libelleFichier, gbc) ;
+		
+		// Bouton Play/Pause
+		this.lecture = new JButton("l>");
+		gbc.gridx = 0 ; gbc.gridy = 20 ;
+		gbc.anchor = GridBagConstraints.PAGE_END ;
+		gbc.insets = new Insets (0, 0, 0, 0) ;
+		this.add (this.lecture, gbc) ;
+
+		// Bouton Stop
+		this.stop = new JButton("[]");
+		gbc.gridwidth = 3 ;
+		gbc.anchor = GridBagConstraints.LAST_LINE_START ;
+		gbc.insets = new Insets (0, 120, 0, 0) ;
+		this.add (this.stop, gbc) ;
+
+		// Bouton Plein écran
+		this.pleinEcran = new JButton("X");
+		gbc.gridwidth = 3 ;
+		gbc.anchor = GridBagConstraints.LAST_LINE_END ;
+		gbc.insets = new Insets (0, 0, 0, 120) ;
+		this.add (this.pleinEcran, gbc) ;
 	}
-	
+
 	/**
 	 * Getter du contrôleur.
 	 * @return Le contrôleur.
@@ -34,4 +95,59 @@ public class Vue_Lecture extends JPanel
 		return this.c;
 	}
 
+	public SelectionTemps getSelecTps() {
+		return selecTps;
+	}
+
+	public void setSelecTps(SelectionTemps selecTps) {
+		this.selecTps = selecTps;
+	}
+
+	public JComboBox getTypeStrategie() {
+		return typeStrategie;
+	}
+
+	public void setTypeStrategie(JComboBox typeStrategie) {
+		this.typeStrategie = typeStrategie;
+	}
+
+	public JComboBox getChoixEquipe() {
+		return choixEquipe;
+	}
+
+	public void setChoixEquipe(JComboBox choixEquipe) {
+		this.choixEquipe = choixEquipe;
+	}
+
+	public JLabel getLibelleFichier() {
+		return libelleFichier;
+	}
+
+	public void setLibelleFichier(JLabel libelleFichier) {
+		this.libelleFichier = libelleFichier;
+	}
+
+	public JButton getLecture() {
+		return lecture;
+	}
+
+	public void setLecture(JButton lecture) {
+		this.lecture = lecture;
+	}
+
+	public JButton getStop() {
+		return stop;
+	}
+
+	public void setStop(JButton stop) {
+		this.stop = stop;
+	}
+
+	public JButton getPleinEcran() {
+		return pleinEcran;
+	}
+
+	public void setPleinEcran(JButton pleinEcran) {
+		this.pleinEcran = pleinEcran;
+	}
 }
