58d2d43e440b3c33b7ada95f1dfa86e7a361e87d
Scytes <tanatosdu31@hotmail.fr>
2013-01-30 14:26:00 +0100

    Modification boutons Vue_Lecture

--- src/v_vues/Vue_Lecture.java
+++ src/v_vues/Vue_Lecture.java
@@ -2,6 +2,7 @@ package v_vues;
 
 import c.Controleur;
 import java.awt.*;
+
 import javax.swing.*;
 import v_utilitaires.SelectionTemps;
 
@@ -17,7 +18,7 @@ public class Vue_Lecture extends JPanel
 	// Composants de la fenêtre
 	private SelectionTemps selecTps ;
 	private JLabel libelleFichier ;
-	private JButton  lecture, stop, pleinEcran;
+	private JButton  lecture, debut, pleinEcran;
 
 	/**
 	 * Création du JFrame et de tous ses composants.
@@ -49,26 +50,37 @@ public class Vue_Lecture extends JPanel
 		gbc.insets = new Insets (0, 0, 0, 0) ;
 		this.add (this.libelleFichier, gbc) ;
 		
+		JPanel boutons = new JPanel (new FlowLayout ()) ;
+
+		// Bouton Début
+		this.debut = new JButton("Début");
+//		gbc.gridwidth = 3 ;
+//		gbc.anchor = GridBagConstraints.LAST_LINE_START ;
+//		gbc.insets = new Insets (0, 120, 0, 0) ;
+//		this.add (this.stop, gbc) ;
+		boutons.add (this.debut) ;
+		
 		// Bouton Play/Pause
-		this.lecture = new JButton("l>");
-		gbc.gridx = 0 ; gbc.gridy = 20 ;
-		gbc.anchor = GridBagConstraints.PAGE_END ;
-		gbc.insets = new Insets (0, 0, 0, 0) ;
-		this.add (this.lecture, gbc) ;
-
-		// Bouton Stop
-		this.stop = new JButton("||");
-		gbc.gridwidth = 3 ;
-		gbc.anchor = GridBagConstraints.LAST_LINE_START ;
-		gbc.insets = new Insets (0, 120, 0, 0) ;
-		this.add (this.stop, gbc) ;
-
+		this.lecture = new JButton("Play/Pause");
+//		gbc.gridx = 0 ; gbc.gridy = 20 ;
+//		gbc.anchor = GridBagConstraints.PAGE_END ;
+//		gbc.insets = new Insets (0, 0, 0, 0) ;
+//		this.add (this.lecture, gbc) ;
+		boutons.add (this.lecture) ;
+		
 		// Bouton Plein écran
-		this.pleinEcran = new JButton("X");
-		gbc.gridwidth = 3 ;
-		gbc.anchor = GridBagConstraints.LAST_LINE_END ;
-		gbc.insets = new Insets (0, 0, 0, 120) ;
-		this.add (this.pleinEcran, gbc) ;
+		this.pleinEcran = new JButton("Plein écran");
+//		gbc.gridwidth = 3 ;
+//		gbc.anchor = GridBagConstraints.LAST_LINE_END ;
+//		gbc.insets = new Insets (0, 0, 0, 120) ;
+//		this.add (this.pleinEcran, gbc) ;
+		boutons.add (this.pleinEcran) ;
+		
+		gbc.gridx = 0 ; gbc.gridy = 2 ;
+		gbc.gridwidth = GridBagConstraints.REMAINDER ; gbc.gridheight = 3 ;
+		gbc.anchor = GridBagConstraints.CENTER ;
+		gbc.insets = new Insets (0, 0, 0, 0) ;
+		this.add (boutons, gbc) ;
 	}
 
 	/**
@@ -105,11 +117,11 @@ public class Vue_Lecture extends JPanel
 	}
 
 	public JButton getStop() {
-		return stop;
+		return debut;
 	}
 
 	public void setStop(JButton stop) {
-		this.stop = stop;
+		this.debut = stop;
 	}
 
 	public JButton getPleinEcran() {
